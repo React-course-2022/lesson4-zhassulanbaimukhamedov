d3aeb3c10da7955108bc33d10fcd364dcd331a00
zhassulanbaimukhamedov <48639707+zhassulanbaimukhamedov@users.noreply.github.com>
2022-02-09 13:17:50 +0600

    Update States.js

--- src/components/States.js
+++ src/components/States.js
@@ -2,17 +2,45 @@ import React, { useState } from "react";
 
 function States() {
   const [products, setProducts] = useState([
-    { id: 1, title: "Milk", color: "red" },
-    { id: 2, title: "Bread", color: "red" },
-    { id: 3, title: "Tea", color: "red" },
-    { id: 4, title: "IceCream", color: "red" },
+    {
+      id: 1,
+      title: "Milk",
+      color: "red",
+      fontSize: "14px",
+      fontWeight: "normal"
+    },
+    {
+      id: 2,
+      title: "Bread",
+      color: "red",
+      fontSize: "14px",
+      fontWeight: "normal"
+    },
+    {
+      id: 3,
+      title: "Tea",
+      color: "red",
+      fontSize: "14px",
+      fontWeight: "normal"
+    },
+    {
+      id: 4,
+      title: "IceCream",
+      color: "red",
+      fontSize: "14px",
+      fontWeight: "normal"
+    }
   ]);
 
   const changeColor = (elem) => {
     const newArray = products.map((item) => {
       item.color = "red";
+      item.fontSize = "14px";
+      item.fontWeight = "normal";
       if (item.id === elem.id) {
-        item.color = "green";
+        item.color = "blue";
+        item.fontSize = "16px";
+        item.fontWeight = "bold";
       }
       return item;
     });
@@ -24,8 +52,16 @@ function States() {
       {products.map((item) => (
         <div
           onClick={() => changeColor(item)}
-          style={{ border: "1px solid grey", padding: "20px", color: item.color }}
-          key={item.id}>
+          style={{
+            cursor: "pointer",
+            border: "1px solid grey",
+            padding: "20px",
+            color: item.color,
+            fontSize: item.fontSize,
+            fontWeight: item.fontWeight
+          }}
+          key={item.id}
+        >
           {item.title}
         </div>
       ))}
